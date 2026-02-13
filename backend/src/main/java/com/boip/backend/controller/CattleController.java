4fd65724cd3ab6715ddef57adfdae58741d4142a
João Marcelo <joaomarsil4@gmail.com>
2026-02-13 15:17:22 -0500

    GET added for CattleLot and change in CattleService to be more readable

--- backend/src/main/java/com/boip/backend/controller/CattleController.java
+++ backend/src/main/java/com/boip/backend/controller/CattleController.java
@@ -11,16 +11,27 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/lots")
 public class CattleController {
     private final CattleService cattleService;
 
-    @PostMapping("/lots")
+    @PostMapping
     public CattleLotResponseDto createLot(@Valid @RequestBody CattleLotCreateRequestDto req) {
         return cattleService.createLot(req);
+    }
+
+    @GetMapping("/{id}")    
+    public CattleLotResponseDto readLot(@PathVariable UUID id){
+        return cattleService.check(id);
     }
     
 }
--- backend/src/main/java/com/boip/backend/service/CattleService.java
+++ backend/src/main/java/com/boip/backend/service/CattleService.java
@@ -1,6 +1,7 @@
 package com.boip.backend.service;
 
 import java.time.OffsetDateTime;
+import java.util.UUID;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -23,8 +24,8 @@ public class CattleService {
         String lotCode = req.getLotCode().trim();
         Integer headCount = req.getHeadCount();
         
-        if (lotCode.length() > 60)
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Must be less than or equal 60 digits");
+        if (lotCode.length() > 60 || lotCode.isBlank())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Must be greater than 0 and less than or equal 60 digits");
 
         if (headCount == null || headCount <= 0)
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Must be at greater than 0");
@@ -44,26 +45,30 @@ public class CattleService {
             .build();
 
         try {
-            CattleLot saved = cattleRepository.saveAndFlush(cattleEntity);
-
-            return CattleLotResponseDto.builder()
-                .id(saved.getId())
-                .ownerUserId(saved.getOwnerUserId())
-                .lotCode(saved.getLotCode())
-                .headCount(saved.getHeadCount())
-                .locationId(saved.getLocationId())
-                .createdAt(saved.getCreatedAt())
-                .updatedAt(saved.getUpdatedAt())
-                .build();
+            CattleLot saved = cattleRepository.save(cattleEntity);
+            return toDto(saved);
         } catch (DataIntegrityViolationException e) {
             // catches FK location_id not found, or unique constraints if race condition
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "invalid data (check locationId / uniqueness)", e);
         }
-
     }
 
+    public CattleLotResponseDto check(UUID id){
+            CattleLot cattleEntity = cattleRepository.findById(id)
+            .orElseThrow((() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "CattleLot not found with id: " + id)));
+            
+            return toDto(cattleEntity);
+    }
 
-
-
-
+    private CattleLotResponseDto toDto(CattleLot cattleEntity){
+        return CattleLotResponseDto.builder()
+                .id(cattleEntity.getId())
+                .ownerUserId(cattleEntity.getOwnerUserId())
+                .lotCode(cattleEntity.getLotCode())
+                .headCount(cattleEntity.getHeadCount())
+                .locationId(cattleEntity.getLocationId())
+                .createdAt(cattleEntity.getCreatedAt())
+                .updatedAt(cattleEntity.getUpdatedAt())
+                .build();
+    }
 }
